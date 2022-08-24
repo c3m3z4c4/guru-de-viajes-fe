18c9688ba26763cd057764a26bc2eeee441ec4bb
C3M3Z4 <contacto@cesarmeza.com>
2022-08-23 22:02:52 -0500

    Changes on home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -13,7 +13,8 @@ import {
   InputNumber,
 } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {instance} from '../../utils'
 const { Header, Content, Footer } = Layout;
 
 
@@ -34,128 +35,146 @@ const renderTitle = (title) => (
 );
 
 
-const renderItem = (title, count) => ({
-  value: title,
+const renderItem = (code, city) => ({
+  value: code,
   label: (
     <div
       style={{
         display: 'flex',
         justifyContent: 'space-between',
       }}
-    >
-      {title}
+    > <strong>
+        {city}
+      </strong>
       <span>
-        <UserOutlined /> {count}
+       {code}
       </span>
     </div>
   ),
 });
 
 
-const options = [
-  {
-    label: renderTitle('Libraries'),
-    options: [renderItem('AntDesign', 10000), renderItem('AntDesign UI', 10600)],
-  },
-  {
-    label: renderTitle('Solutions'),
-    options: [renderItem('AntDesign UI FAQ', 60100), renderItem('AntDesign FAQ', 30010)],
-  },
-  {
-    label: renderTitle('Articles'),
-    options: [renderItem('AntDesign design language', 100000)],
-  },
-];
 
 
 const { RangePicker } = DatePicker;
 
-const Home = () => (
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        items={new Array(15).fill(null).map((_, index) => {
-          const key = index + 1;
-          return {
-            key,
-            label: `nav ${key}`,
-          };
-        })}
-      />
-    </Header>
-    <Content
-      style={{
-        padding: '0 50px',
-      }}
-    >
-      <Breadcrumb
+const Home = () => {
+  const [location, setLocation] = useState([]);
+  const [departure, setDeparture] = useState('');
+  const [destination, setDestination] = useState('');
+
+
+  let options = location.map((elem) => (
+    {
+     options: [renderItem(elem.code, elem.city )],
+    }
+  ))
+
+  
+  useEffect(() => {
+    console.log(`Sale desde ${departure}`);
+    console.log(destination)
+},[destination])
+
+
+  useEffect(() => {
+    instance.get('/')
+      .then((response) => {
+        let data = response.data.filteredAirports
+        setLocation([...data])
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+   
+  },[]);
+ console.log(location)
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content
         style={{
-          margin: '16px 0',
-        }
-      
-      
-        }
+          padding: '0 50px',
+        }}
       >
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">
-        <Space direction="vertical">
-          <Space direction="horizontal">
-          <AutoComplete
-            dropdownClassName="certain-category-search-dropdown"
-            dropdownMatchSelectWidth={500}
-            style={{
-              width: 250,
-            }}
-            options={options} >
-            <Input.Search size="large" placeholder="De donde viajas?" />
-          </AutoComplete>
-
-          <AutoComplete
-            dropdownClassName="certain-category-search-dropdown"
-            dropdownMatchSelectWidth={500}
-            style={{
-              width: 250,
-            }}
-            options={options} >
-            <Input.Search size="large" placeholder="A donde vas?" />
-            </AutoComplete>
-        </Space>
-        <div>
-          <Space direction="vertical" size={12}>
-            <RangePicker />
-          </Space>
-        </div>
-        <div>
-        <Space direction="horizontal">
-
-          <InputNumber addonBefore="Adultos" defaultValue={1} />
-            <InputNumber addonBefore="Ninos" defaultValue={0} />
-          </Space>
-        </div>
-      </Space>
-      </div>
-
-
-         
-
-
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }
       
-    </Content>
-    <Footer
-      style={{
-        textAlign: 'center',
-      }}
-    >
-      Ant Design ©2018 Created by Ant UED
-    </Footer>
-  </Layout>
-);
+      
+          }
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <Space direction="vertical">
+            <Space direction="horizontal">
+              <AutoComplete
+                dropdownClassName="certain-category-search-dropdown"
+                dropdownMatchSelectWidth={500}
+                style={{
+                  width: 250,
+                }}
+                options={options} >
+                <Input.Search size="large" placeholder="De donde viajas?" onSelect={(event) => {
+                  setDeparture(event.target.value)
+                }} />
+              </AutoComplete>
+
+              <AutoComplete
+                dropdownClassName="certain-category-search-dropdown"
+                dropdownMatchSelectWidth={500}
+                style={{
+                  width: 250,
+                }}
+                options={options} >
+                <Input.Search size="large" placeholder="A donde vas?" onSelect={(event) => {
+                  setDestination(event.target.value)
+                }}  />
+              </AutoComplete>
+            </Space>
+            <div>
+              <Space direction="vertical" size={12}>
+                <RangePicker />
+              </Space>
+            </div>
+            <div>
+              <Space direction="horizontal">
+
+                <InputNumber addonBefore="Adultos" defaultValue={1} />
+                <InputNumber addonBefore="Ninos" defaultValue={0} />
+              </Space>
+            </div>
+          </Space>
+        </div>
+
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  )
+};
 
 export default Home;
